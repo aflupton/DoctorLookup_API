b487b6d851c8ea268ea29a71d8ce9ef358f582f9
andrew lupton <aflupton@gmail.com>
2018-07-24 16:42:02 -0700

    add changes to api handling, still need to fix submit and display handling

--- src/main.js
+++ src/main.js
@@ -8,22 +8,18 @@ $(document).ready(function() {
   $('#statusError').hide();
   $('#postError').hide();
   $('#resultsTable').hide();
-  $('#doctorForm').submit(function(event) {
+  $('#inputForm').submit(function(event) {
     event.preventDefault();
 
     // store user input data in variables
-    let location = $('#location').val();
     let name = $('#name').val();
-    let doctor = new Doctor(location, name);
-
-    $('#location').val("");
     $('#name').val("");
+    let doctor = new Doctor(name);
 
     // // instantiate new XML request
     let request = new XMLHttpRequest();
     // store api url in a variable
-    let url = `http://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=${location}&sort=rating-desc&skip=0&limit=25&user_key=3b4a4ee331e13503bf566bdda530b283`;
-    // replace key with ${process.env.exports.apiKey}
+    let url = `http://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.608%2C-122.335%2C30&sort=rating-desc&skip=0&limit=25&user_key=${process.env.exports.apikey}`;
 
     // verify request
     request.onreadystatechange = function() {
@@ -43,33 +39,37 @@ $(document).ready(function() {
 
     // post results to html page
     let getElements = function(response) {
-      if(response.data != 0) {
+      if(response.data != "") {
         // show results header and div
-        $('#resultsHeader').text(`Showing doctors from ${location}:`);
+        $('#resultsHeader').text(`Showing doctor(s) with name ${name} in your area:`);
         $('#resultsTable').show();
         // api data
         for(let i=0; i<response.data.length; i++) {
-          alert("hi");
-          $('#results').append(`
-            <tr>
-            <td><img src=${response.data[i].profile.img_url}></img></td>
-            <td>${response.data[i].practices.name}</td>
-            <td>${response.data[i].profile.first_name}</td>
-            <td>${response.data[i].profile.last_name}</td>
-            <td>${response.data[i].profile.title}</td>
-            <td>${response.data[i].profile.data[i].practices.name}</td>
-            <td>${response.data[i].profile.data[i].practices.visit_address.street}
-            <br>
-            ${response.data[i].profile.data[i].practices.visit_address.city}
-            <br>
-            ${response.data[i].profile.data[i].practices.visit_address.state}
-            <br>
-            ${response.data[i].profile.data[i].practices.visit_address.zip}
-            </td>
-            <td>${response.data[i].practices.website}</td>
-            <td>${response.data[i].profile.accepts_new_patients}</td>
-            </tr>
-            `)
+          response.data[0].practices.forEach(function(practiceInfo) {
+            response.data[0].specialties.forEach(function(bio) {
+              $('#results').append(`
+                <tr>
+                <td><img src=${response.data[i].profile.img_url}></img></td>
+                <td>${response.data[i].practices.name}</td>
+                <td>${response.data[i].profile.first_name}</td>
+                <td>${response.data[i].profile.last_name}</td>
+                <td>${response.data[i].profile.title}</td>
+                <td>${response.data[i].profile.data[i].practices.name}</td>
+                <td>${response.data[i].profile.data[i].practices.visit_address.street}
+                <br>
+                ${response.data[i].profile.data[i].practices.visit_address.city}
+                <br>
+                ${response.data[i].profile.data[i].practices.visit_address.state}
+                <br>
+                ${response.data[i].profile.data[i].practices.visit_address.zip}
+                </td>
+                <td>${response.data[i].practices.website}</td>
+                <td>${response.data[i].profile.accepts_new_patients}</td>
+                </tr>
+                `)
+            })
+          })
+
         }
       } else {
         $('#postError').show();
@@ -77,7 +77,7 @@ $(document).ready(function() {
       }
     }
   });
-  $('#specialtyForm').submit(function(event) {
+  $('#inputForm').submit(function(event) {
     event.preventDefault();
     let query = $('#query').val();
 
@@ -85,8 +85,7 @@ $(document).ready(function() {
 
     let request = new XMLHttpRequest();
     // store api url in a variable
-    let url = `http://api.betterdoctor.com/2016-03-01/doctors?&query=${query}&sort=rating-desc&skip=0&limit=25&user_key=3b4a4ee331e13503bf566bdda530b283`;
-    // replace key with ${process.env.exports.apiKey}
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
     request.onreadystatechange = function() {
       if(this.readystate === 4 && this.status === 200) {
