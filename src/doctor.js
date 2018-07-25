a22e58580634e96e8694087d92e29b4bf61c27f3
andrew lupton <aflupton@gmail.com>
2018-07-25 14:41:57 -0700

    fix bugs in main.js

--- src/doctor.js
+++ src/doctor.js
@@ -1,25 +1,7 @@
 export class Doctor {
-  constructor (location, query) {
-    this.location = location;
+  constructor (name, query) {
+    this.name = name;
     this.query = query;
   }
-  // // store a new xml request in a variable
-  // search(elements) {
-  //   let request = new XMLHttpRequest();
-  //   let url = `http://api.betterdoctor.ccom/2016-03-01/doctors?query=${this.query}&location=${this.location}&sort=rating-desc&skip=0&limit=25&user_key=3b4a4ee331e13503bf566bdda530b283`;
-  //   // replace key with ${process.env.exports.apiKey}
-  //   //check connection to the api
-  //   request.onreadystatechange = function() {
-  //     if(this.readyState === 4 && this.status === 200) {
-  //       let response = JSON.parse(this.responseText);
-  //       getElements(response);
-  //     } else if (this.readyState == 4 && this.status != 200) {
-  //       $('#statusError').show();
-  //       $('#statusError').text('There was a problem with your request, refresh and try again.');
-  //     }
-  //   }
-  //   // open and send request
-  //   request.open("GET", url, true);
-  //   request.send();
-  // }
+
 }
--- src/main.js
+++ src/main.js
@@ -2,137 +2,55 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { Doctor } from './doctor.js';
+// import { Doctor } from './doctor.js';
 
 $(document).ready(function() {
-  $('#statusError').hide();
   $('#postError').hide();
-  $('#resultsTable').hide();
+  $('#results').hide();
   $('#inputForm').submit(function(event) {
     event.preventDefault();
 
-    // store user input data in variables
-    let name = $('#name').val();
-    $('#name').val("");
-    let doctor = new Doctor(name);
+    let name = $("#name").val();
+    let query = $("#query").val();
+    $("#name").val("");
+    $("#query").val("");
 
-    // // instantiate new XML request
     let request = new XMLHttpRequest();
-    // store api url in a variable
-    let url = `http://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.608%2C-122.335%2C30&sort=rating-desc&skip=0&limit=25&user_key=${process.env.exports.apikey}`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=47.6062%2C-122.3321%2C100&sort=rating-desc&skip=0&limit=50&user_key=${process.env.API_KEY}`;
 
-    // verify request
     request.onreadystatechange = function() {
-      if(this.readystate === 4 && this.status === 200) {
+      if (this.readyState === 4 && this.status === 200) {
         let response = JSON.parse(this.responseText);
         getElements(response);
-      } else if (this.readyState === 4 && this.status != 200) {
-        alert("ready state: " + this.readyState + " status: " + this.status);
-        $('#statusError').show();
-        $('#statusError').text('There was an error with your request, check your API key before trying again.');
+      } else if (this.readyState === 4 && this.status !== 200) {
+        $("#statusError").text('There is a problem with your request, please try again.');
+      } else {
+        $("#statusError").text('Please wait, request is processing.');
       }
     }
 
-    // open a get request
+    // open get request
     request.open("GET", url, true);
     request.send();
 
-    // post results to html page
+    // post response
     let getElements = function(response) {
-      if(response.data != "") {
-        // show results header and div
-        $('#resultsHeader').text(`Showing doctor(s) with name ${name} in your area:`);
-        $('#resultsTable').show();
-        // api data
-        for(let i=0; i<response.data.length; i++) {
-          response.data[0].practices.forEach(function(practiceInfo) {
-            response.data[0].specialties.forEach(function(bio) {
-              $('#results').append(`
-                <tr>
-                <td><img src=${response.data[i].profile.img_url}></img></td>
-                <td>${response.data[i].practices.name}</td>
-                <td>${response.data[i].profile.first_name}</td>
-                <td>${response.data[i].profile.last_name}</td>
-                <td>${response.data[i].profile.title}</td>
-                <td>${response.data[i].profile.data[i].practices.name}</td>
-                <td>${response.data[i].profile.data[i].practices.visit_address.street}
-                <br>
-                ${response.data[i].profile.data[i].practices.visit_address.city}
-                <br>
-                ${response.data[i].profile.data[i].practices.visit_address.state}
-                <br>
-                ${response.data[i].profile.data[i].practices.visit_address.zip}
-                </td>
-                <td>${response.data[i].practices.website}</td>
-                <td>${response.data[i].profile.accepts_new_patients}</td>
-                </tr>
-                `)
-            })
-          })
-
-        }
+      if (response.data != "") {
+        $("#results").show();
+        $("#resultsHeader").text(`Here are the doctors with the following name: ${name} and search criteria ${query}.`);
+        response.data.forEach(function(practices) {
+          $("span#resultsList").append(`
+            <ul>
+              <li><h3>${practices.profile.last_name}, ${practices.profile.first_name}</h3></li>
+              <li>${practices.phones.number}</li>
+              <li>${practices.visit_address.street}, ${practices.visit_address.city}, ${practices.visit_address.state}, ${practices.visit_address.zip}</li>
+            </ul>
+            `);
+        });
       } else {
-        $('#postError').show();
-        $('#postError').text('There were no results matching your search criteria, refresh and try entering new values.');
+        $("#postError").show();
+        $("#postError").text("Your query did not return any doctors. Refresh and try again with a new search parameter.");
       }
-    }
-  });
-  $('#inputForm').submit(function(event) {
-    event.preventDefault();
-    let query = $('#query').val();
-
-    $('#query').val("");
-
-    let request = new XMLHttpRequest();
-    // store api url in a variable
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-
-    request.onreadystatechange = function() {
-      if(this.readystate === 4 && this.status === 200) {
-        let response = JSON.parse(this.responseText);
-        getElements(response);
-      } else if (this.readyState === 4 && this.status != 200) {
-        $('#statusError').show();
-        $('#statusError').text('There was an error with your request, check your API key before trying again.');
-      }
-    }
-
-    let getElements = function(response) {
-      if(response.data != 0) {
-        // show results header and div
-        $('#resultsHeader').text(`Showing doctors thattreat ${query}:`);
-        $('#resultsTable').show();
-        // api data
-        for(let i=0; i<response.data.length; i++) {
-          $('#results').append(`
-            <tr>
-            <td><img src=${response.data[i].profile.img_url}></img></td>
-            <td>${response.data[i].practices.name}</td>
-            <td>${response.data[i].profile.first_name}</td>
-            <td>${response.data[i].profile.last_name}</td>
-            <td>${response.data[i].profile.title}</td>
-            <td>${response.data[i].profile.data[i].practices.name}</td>
-            <td>${response.data[i].profile.data[i].practices.visit_address.street}
-            <br>
-            ${response.data[i].profile.data[i].practices.visit_address.city}
-            <br>
-            ${response.data[i].profile.data[i].practices.visit_address.state}
-            <br>
-            ${response.data[i].profile.data[i].practices.visit_address.zip}
-            </td>
-            <td>${response.data[i].practices.website}</td>
-            <td>${response.data[i].profile.accepts_new_patients}</td>
-            </tr>
-            `)
-        }
-      } else {
-        $('#postError').show();
-        $('#postError').text('There were no results matching your search criteria, refresh and try entering new values.');
-      }
-    }
-  });
-  $('#refresh').click(function(event) {
-    event.preventDefault();
-    location.reload();
+    };
   });
 });
